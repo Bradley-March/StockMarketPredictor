94b9eec54e0d2a2ff24f416112c0cb151e82b743
Bradley-March <101671720+Bradley-March@users.noreply.github.com>
2024-07-28 16:56:25 +0100

    Added code to read the data in and add new columns for daily % change and daily spread %

--- data_production.py
+++ data_production.py
@@ -12,10 +12,48 @@ from time import time
 mpl.use("TkAgg")
 plt.ion()
 
+tstart = time() # start time for the script
+
 #%% Load data into Python
 # Data from https://www.kaggle.com/datasets/pavankrishnanarne/global-stock-market-2008-present
 
 years_included = range(2008, 2023) # max range 2008 -> 2023
 data = pd.read_csv(osjoin("Global_Market_Data", str(years_included[0]) + "_Global_Markets_Data.csv"))
-for year in years_included[1:]: # join each years data into one dataframe
+for year in years_included[1:]: # combine every year into one dataframe
     data = pd.concat([data, pd.read_csv(osjoin("Global_Market_Data", str(year) + "_Global_Markets_Data.csv"))], ignore_index=True) 
+
+print(data.groupby("Ticker").size().sort_values(ascending=False)) # print number of entries for each ticker 
+
+#%% Create a subset of the data for a single ticker
+
+# select a single ticker
+market = "^FTSE"
+market_data = data[data["Ticker"] == market]
+
+#%% Modifying the data
+
+# drop irrelevant columns
+market_data = market_data.drop(columns=["Adj Close"])
+
+# convert date to datetime
+data['Date'] = pd.to_datetime(data['Date'])
+# sort by date
+data = data.sort_values("Date").reset_index(drop=True)
+
+#%% Convert daily price to daily percentage change
+
+# calculate daily percentage change
+market_data["Daily Return"] = market_data["Close"].pct_change()
+
+# drop the first row as it will be NaN
+market_data = market_data.dropna()
+
+#%% Add a columns for the spread between the high and low price
+
+# calculate the spread
+market_data["Spread"] = (market_data["High"] - market_data["Low"]) / market_data["Close"]
+
+
+#%% Final timings 
+tend = time() # end time for the script
+print(f"Time taken: {tend - tstart:.2f} seconds") # print time taken for the script to run
